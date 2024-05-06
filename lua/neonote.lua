ac1687b0b681da773610b8b6f2bdd2298a51e73a
fschlegelone <fabian@schlegel.one>
2024-05-06 12:58:51 +0200

    feat: Setup & Select function

--- lua/neonote.lua
+++ lua/neonote.lua
@@ -1,23 +1,35 @@
-local telescope = require("telescope")
-local actions = require("telescope.actions")
+local M = {}
 
-local function main()
-	print("Hello from our plugin")
+-- default notes_dir
+M.note_dir = "~/notes"
+
+-- setup function
+M.setup = function(config)
+	M.note_dir = config.note_dir or M.note_dir
 end
 
-local function setup()
-	vim.cmd("command! Neonote lua require('neonote').neonote_command()")
+-- telescope integration
+M.open_telescope = function()
+	local telescope = require("telescope.builtin")
+
+	-- List all .md files in the note directory
+	local files = vim.fn.glob(M.note_dir .. "/*.md", true, true)
+
+	-- Use Telescope to display the list of files
+	telescope.file_browser({
+		prompt_title = "Notes",
+		cwd = M.note_dir,
+		hidden = true,
+		attach_mappings = function(_, map)
+			-- Custom mapping to open selected file in a new buffer
+			map("i", "<CR>", function(bufnr)
+				local entry = require("telescope.actions.state").get_selected_entry()
+				require("telescope.actions").close(bufnr)
+				vim.cmd("edit " .. entry.path)
+			end)
+			return true
+		end,
+	})
 end
 
-local function neonote_command()
-	telescope
-		.find_files({
-			prompt_title = "Neonote - Select .md files",
-			cwd = vim.fn.expand("<sfile>:p:h"), -- Open Telescope in the directory of the current Lua file
-			file_ignore_patterns = { "*.git/*" }, -- Ignore git directories
-			find_command = { "fd", "--type", "f", "--extension", "md" }, -- Use fd as the finder command
-		})
-		:find()
-end
-
-return { setup = setup, neonote_command = neonote_command }
+return M
