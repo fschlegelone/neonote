3c815f7f376ddd285cf390e18218268081902aee
fschlegelone <fabian@schlegel.one>
2024-05-06 13:59:20 +0200

    modify setup function

--- lua/neonote/init.lua
+++ lua/neonote/init.lua
@@ -1,15 +1,29 @@
 local M = {}
 local telescope = require("telescope.builtin")
-local notes_dir = "~/notes/neonote"
+
+local default_config = {
+	notes_dir = "~/notes",
+	tasks_dir = "~/notes/tasks",
+	include_files = { ".md", ".txt" },
+}
+
+local config = {}
 
 function M.open()
 	telescope.find_files({
 		prompt_title = "Neonote",
-		cwd = notes_dir,
+		cwd = config.notes_dir,
 	})
 end
 
-function M.setup()
+function M.setup(user_config)
+	if user_config then
+		for key, value in pairs(user_config) do
+			config[key] = value
+		end
+	else
+		config = default_config
+	end
 	vim.api.nvim_set_keymap(
 		"n",
 		"<leader>nn",
