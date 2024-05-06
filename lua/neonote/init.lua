b1756556d6e36f165e04ffbf7f2657cdabc8c0a2
fschlegelone <fabian@schlegel.one>
2024-05-06 10:57:39 +0200

    feat: Setup & Select function

---
+++ lua/neonote/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local note_dir = ""
+
+-- setup function
+function M.setup(opts)
+	note_dir = opts.note_dir or ""
+end
+
+function M.select_notes()
+	local notes = vim.fn.glob(note_dir .. "*.md", true, true)
+	local selected_note = vim.fn.inputlist(notes)
+	if selected_note ~= 0 then
+		vim.cmd("e " .. notes[selected_note])
+	end
+end
+
+return M
