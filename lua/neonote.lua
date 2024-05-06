4dc94b736b5c9376b7ce1dddde30750ae1a18715
fschlegelone <fabian@schlegel.one>
2024-05-06 12:27:08 +0200

    feat: Setup & Select function

--- lua/neonote.lua
+++ lua/neonote.lua
@@ -6,20 +6,10 @@ local function main()
 end
 
 local function setup()
-	local augroup = vim.api.nvim_create_augroup("Neonote", { clear = true })
-
-	vim.api.nvim_create_autocmd(
-		"VimEnter",
-		{ group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = main }
-	)
-
-	-- Define the :neonote command
 	vim.cmd("command! Neonote lua require('neonote').neonote_command()")
 end
 
--- Function to execute the command
 local function neonote_command()
-	-- Setup Telescope to select .md files
 	telescope
 		.find_files({
 			prompt_title = "Neonote - Select .md files",
